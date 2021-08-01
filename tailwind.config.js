58efd086b364dd71c635f9e268762942da30a9ed
behnami454 <behnami454@gmail.com>
2021-08-01 17:44:00 +0430

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        one:'#324532'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
